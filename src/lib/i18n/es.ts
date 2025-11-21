f200987fa1cdf7f4e894310600bb1c9e676a00a0
zkjon <121103151+zkj0n@users.noreply.github.com>
2025-11-21 11:11:41 +0100

    2.2.0: fix: text quality fixed

--- src/lib/i18n/es.ts
+++ src/lib/i18n/es.ts
@@ -47,7 +47,7 @@ export const es: Translations = {
     seeDetails: "Ver detalles",
     close: "Cerrar",
     qualityText:
-      "Cada producto Fruco es elaborado con los más altos estándares de calidad, utilizando ingredientes seleccionados para ofrecerte una experiencia gastronómica excepcional.",
+      "Cada producto Fruco se elabora con los más altos estándares de calidad, utilizando ingredientes seleccionados para ofrecerte una experiencia gastronómica excepcional.",
     items: {
       frito: {
         name: "Tomate frito",
